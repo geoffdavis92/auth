ae5fc7557b5c67c5195fa18645be419712bee6f6
geoffdavis92 <gdavis@archermalmo.com>
2015-08-20 14:53:00 -0500

    GSI update 02:52:57 PM

--- script/es6/index.js
+++ script/es6/index.js
@@ -1,12 +1,17 @@
 // Index.js
 let gsi = document.querySelector('google-signin')
+gsi.click(function(e) {
+	gsi.signIn()
+});
 const API_KEY = "AIzaSyDsZhCsjTtHrLuLRPQKYfs7AaS3js8TIyw"
 const CLIENT_ID = "844275130627-7o60j8u9qoe3rj50m5sib5m7ik8rig9q.apps.googleusercontent.com"
-$.ajax({
-	url: 'https://www.googleapis.com/plusDomains/v1/people/me',
-	data: `${API_KEY}`,
-	dataType: 'json',
-	method: 'GET'
-}).done(function(data) {
-	console.log(data)
-})
+function getData () {
+	$.ajax({
+		url: 'https://www.googleapis.com/plusDomains/v1/people/me',
+		data: `${API_KEY}`,
+		dataType: 'json',
+		method: 'GET'
+	}).done(function(data) {
+		console.log(data)
+	})
+}
--- script/index.js
+++ script/index.js
@@ -2,13 +2,18 @@
 "use strict";
 
 var gsi = document.querySelector("google-signin");
+gsi.click(function (e) {
+	gsi.signIn();
+});
 var API_KEY = "AIzaSyDsZhCsjTtHrLuLRPQKYfs7AaS3js8TIyw";
 var CLIENT_ID = "844275130627-7o60j8u9qoe3rj50m5sib5m7ik8rig9q.apps.googleusercontent.com";
-$.ajax({
-	url: "https://www.googleapis.com/plusDomains/v1/people/me",
-	data: "" + API_KEY,
-	dataType: "json",
-	method: "GET"
-}).done(function (data) {
-	console.log(data);
-});
+function getData() {
+	$.ajax({
+		url: "https://www.googleapis.com/plusDomains/v1/people/me",
+		data: "" + API_KEY,
+		dataType: "json",
+		method: "GET"
+	}).done(function (data) {
+		console.log(data);
+	});
+}
