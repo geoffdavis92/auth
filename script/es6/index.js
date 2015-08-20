bd363b58ed05fcddcdc8ac6be9a19686c96ec58e
geoffdavis92 <gdavis@archermalmo.com>
2015-08-20 14:38:43 -0500

    GSI update 02:38:43 PM

--- script/es6/index.js
+++ script/es6/index.js
@@ -1,7 +1,10 @@
 // Index.js
 let gsi = document.querySelector('google-signin')
+const API_KEY = "AIzaSyDsZhCsjTtHrLuLRPQKYfs7AaS3js8TIyw"
+const CLIENT_ID = "844275130627-7o60j8u9qoe3rj50m5sib5m7ik8rig9q.apps.googleusercontent.com"
 $.ajax({
 	url: 'https://www.googleapis.com/plusDomains/v1/people/me',
+	data: `${API_KEY}`,
 	dataType: 'json',
 	method: 'GET'
 }).done(function(data) {
--- script/index.js
+++ script/index.js
@@ -1,11 +1,14 @@
 // Index.js
-'use strict';
+"use strict";
 
-var gsi = document.querySelector('google-signin');
+var gsi = document.querySelector("google-signin");
+var API_KEY = "AIzaSyDsZhCsjTtHrLuLRPQKYfs7AaS3js8TIyw";
+var CLIENT_ID = "844275130627-7o60j8u9qoe3rj50m5sib5m7ik8rig9q.apps.googleusercontent.com";
 $.ajax({
-	url: 'https://www.googleapis.com/plusDomains/v1/people/me',
-	dataType: 'json',
-	method: 'GET'
+	url: "https://www.googleapis.com/plusDomains/v1/people/me",
+	data: "" + API_KEY,
+	dataType: "json",
+	method: "GET"
 }).done(function (data) {
 	console.log(data);
 });
