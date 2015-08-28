ea17afa0427890a042efa961c9fdac8228e2dafa
geoffdavis92 <gdavis@archermalmo.com>
2015-08-28 16:10:38 -0500

    Update script

--- script/es6/index.js
+++ script/es6/index.js
@@ -7,7 +7,7 @@ gsi.click(function(e) {
 const API_KEY = "AIzaSyDsZhCsjTtHrLuLRPQKYfs7AaS3js8TIyw"
 const CLIENT_ID = "844275130627-7o60j8u9qoe3rj50m5sib5m7ik8rig9q.apps.googleusercontent.com"
 
-gsi.addEventListener('google-signin-aware-success', readData(data))
+gsi.addEventListener('google-signin-aware-success', readData())
 
 function readData(data) {
 	let access_token = data.detail.access_token
