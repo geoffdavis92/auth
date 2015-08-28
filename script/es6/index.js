a4f8b83a137ac422d5bd900610609300506242a3
geoffdavis92 <gdavis@archermalmo.com>
2015-08-28 15:59:04 -0500

    Update script

--- script/es6/index.js
+++ script/es6/index.js
@@ -1,10 +1,18 @@
 // Index.js
 let gsi = document.querySelector('google-signin')
+let gsia = document.querySelector('google-signin-aware')
 gsi.click(function(e) {
 	gsi.signIn()
 });
 const API_KEY = "AIzaSyDsZhCsjTtHrLuLRPQKYfs7AaS3js8TIyw"
 const CLIENT_ID = "844275130627-7o60j8u9qoe3rj50m5sib5m7ik8rig9q.apps.googleusercontent.com"
+
+function readData() {
+	gsia.on('google-signin-aware-success', function(data) {
+		console.log(data)
+	})
+}
+
 function getData () {
 	$.ajax({
 		url: 'https://www.googleapis.com/plusDomains/v1/people/me',
--- script/index.js
+++ script/index.js
@@ -1,18 +1,26 @@
 // Index.js
-"use strict";
+'use strict';
 
-var gsi = document.querySelector("google-signin");
+var gsi = document.querySelector('google-signin');
+var gsia = document.querySelector('google-signin-aware');
 gsi.click(function (e) {
 	gsi.signIn();
 });
-var API_KEY = "AIzaSyDsZhCsjTtHrLuLRPQKYfs7AaS3js8TIyw";
-var CLIENT_ID = "844275130627-7o60j8u9qoe3rj50m5sib5m7ik8rig9q.apps.googleusercontent.com";
+var API_KEY = 'AIzaSyDsZhCsjTtHrLuLRPQKYfs7AaS3js8TIyw';
+var CLIENT_ID = '844275130627-7o60j8u9qoe3rj50m5sib5m7ik8rig9q.apps.googleusercontent.com';
+
+function readData() {
+	gsia.on('google-signin-aware-success', function (data) {
+		console.log(data);
+	});
+}
+
 function getData() {
 	$.ajax({
-		url: "https://www.googleapis.com/plusDomains/v1/people/me",
-		data: "" + API_KEY,
-		dataType: "json",
-		method: "GET"
+		url: 'https://www.googleapis.com/plusDomains/v1/people/me',
+		data: 'token=' + CLIENT_ID,
+		dataType: 'json',
+		method: 'GET'
 	}).done(function (data) {
 		console.log(data);
 	});
