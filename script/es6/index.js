851e9166ff6de8519d57a7c82c30f27978f8efce
Geoff Davis <gdavis@archermalmo.com>
2015-09-02 16:37:05 -0500

    Update Javascript | Update 04:37:05 PM

--- script/es6/index.js
+++ script/es6/index.js
@@ -1,61 +1,68 @@
 // Index.js
-const body = document.querySelector('body')
-let gsi = document.querySelector('google-signin')
-let gsia = document.querySelector('google-signin-aware')
+if (window.location.href === 'http://geoffdavis92.github.io/auth/unauthorized.html') {
+    // Close out after 10 seconds
+    let timeout = setTimeout(function() {
+        window.close()
+    }, 5000)
+} else {
+    const body = document.querySelector('body')
+    let gsi = document.querySelector('google-signin')
+    let gsia = document.querySelector('google-signin-aware')
 
-body.style.display = "none"
+    body.style.display = "none"
 
-// gsi.click(function(e) {
-	gsi.signIn()
-// });
+    // gsi.click(function(e) {
+    gsi.signIn()
+        // });
 
-const API_KEY = "AIzaSyDsZhCsjTtHrLuLRPQKYfs7AaS3js8TIyw"
-const CLIENT_ID = "844275130627-7o60j8u9qoe3rj50m5sib5m7ik8rig9q.apps.googleusercontent.com"
+    const API_KEY = "AIzaSyDsZhCsjTtHrLuLRPQKYfs7AaS3js8TIyw"
+    const CLIENT_ID = "844275130627-7o60j8u9qoe3rj50m5sib5m7ik8rig9q.apps.googleusercontent.com"
 
-gsi.addEventListener('google-signin-aware-success', function(data) {
-	readData(data)
-})
+    gsi.addEventListener('google-signin-aware-success', function(data) {
+        readData(data)
+    })
 
-function readData(data) {
-	console.log(data)
-	let access_token = data.detail.access_token
-	let id_token = data.detail.id_token
-	$.ajax({
-		url: `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${access_token}`,
-		success: function(data) {
-			console.log(data)
-			let email = data.email
-			console.log(email)
-			testEmail(email)
-		}
-	}).done( console.log('Finished') )
-}
+    function readData(data) {
+        console.log(data)
+        let access_token = data.detail.access_token
+        let id_token = data.detail.id_token
+        $.ajax({
+            url: `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${access_token}`,
+            success: function(data) {
+                console.log(data)
+                let email = data.email
+                console.log(email)
+                testEmail(email)
+            }
+        }).done(console.log('Finished'))
+    }
 
-function getData () {
-	$.ajax({
-		url: 'https://www.googleapis.com/plusDomains/v1/people/me',
-		data: `token=${CLIENT_ID}`,
-		dataType: 'json',
-		method: 'GET'
-	}).done(function(data) {
-		console.log(data)
-	})
-}
+    function getData() {
+        $.ajax({
+            url: 'https://www.googleapis.com/plusDomains/v1/people/me',
+            data: `token=${CLIENT_ID}`,
+            dataType: 'json',
+            method: 'GET'
+        }).done(function(data) {
+            console.log(data)
+        })
+    }
 
-function testEmail(email) {
-	let url = window.href
-	let domain = (a) => {
-		a = a.split('@')
-		a = a[1].split('.')
-		if (a[0] === 'archermalmo') {
-			return true
-		} else {
-			return false
-		}
-	}
-	if (domain(email)) {
-		body.style.display = 'initial'
-	} else {
-		window.open('unauthorized.html', '_self')
-	}
+    function testEmail(email) {
+        let url = window.href
+        let domain = (a) => {
+            a = a.split('@')
+            a = a[1].split('.')
+            if (a[0] === 'archermalmo') {
+                return true
+            } else {
+                return false
+            }
+        }
+        if (domain(email)) {
+            body.style.display = 'initial'
+        } else {
+            window.open('unauthorized.html', '_self')
+        }
+    }
 }
--- script/index.js
+++ script/index.js
@@ -1,63 +1,72 @@
 // Index.js
 'use strict';
 
-var body = document.querySelector('body');
-var gsi = document.querySelector('google-signin');
-var gsia = document.querySelector('google-signin-aware');
+if (window.location.href === 'http://geoffdavis92.github.io/auth/unauthorized.html') {
+    // Close out after 10 seconds
+    var timeout = setTimeout(function () {
+        window.close();
+    }, 5000);
+} else {
+    (function () {
+        var readData = function readData(data) {
+            console.log(data);
+            var access_token = data.detail.access_token;
+            var id_token = data.detail.id_token;
+            $.ajax({
+                url: 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=' + access_token,
+                success: function success(data) {
+                    console.log(data);
+                    var email = data.email;
+                    console.log(email);
+                    testEmail(email);
+                }
+            }).done(console.log('Finished'));
+        };
 
-body.style.display = "none";
+        var getData = function getData() {
+            $.ajax({
+                url: 'https://www.googleapis.com/plusDomains/v1/people/me',
+                data: 'token=' + CLIENT_ID,
+                dataType: 'json',
+                method: 'GET'
+            }).done(function (data) {
+                console.log(data);
+            });
+        };
 
-// gsi.click(function(e) {
-gsi.signIn();
-// });
+        var testEmail = function testEmail(email) {
+            var url = window.href;
+            var domain = function domain(a) {
+                a = a.split('@');
+                a = a[1].split('.');
+                if (a[0] === 'archermalmo') {
+                    return true;
+                } else {
+                    return false;
+                }
+            };
+            if (domain(email)) {
+                body.style.display = 'initial';
+            } else {
+                window.open('unauthorized.html', '_self');
+            }
+        };
 
-var API_KEY = "AIzaSyDsZhCsjTtHrLuLRPQKYfs7AaS3js8TIyw";
-var CLIENT_ID = "844275130627-7o60j8u9qoe3rj50m5sib5m7ik8rig9q.apps.googleusercontent.com";
+        var body = document.querySelector('body');
+        var gsi = document.querySelector('google-signin');
+        var gsia = document.querySelector('google-signin-aware');
 
-gsi.addEventListener('google-signin-aware-success', function (data) {
-	readData(data);
-});
+        body.style.display = "none";
 
-function readData(data) {
-	console.log(data);
-	var access_token = data.detail.access_token;
-	var id_token = data.detail.id_token;
-	$.ajax({
-		url: 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=' + access_token,
-		success: function success(data) {
-			console.log(data);
-			var email = data.email;
-			console.log(email);
-			testEmail(email);
-		}
-	}).done(console.log('Finished'));
-}
+        // gsi.click(function(e) {
+        gsi.signIn();
+        // });
 
-function getData() {
-	$.ajax({
-		url: 'https://www.googleapis.com/plusDomains/v1/people/me',
-		data: 'token=' + CLIENT_ID,
-		dataType: 'json',
-		method: 'GET'
-	}).done(function (data) {
-		console.log(data);
-	});
-}
+        var API_KEY = "AIzaSyDsZhCsjTtHrLuLRPQKYfs7AaS3js8TIyw";
+        var CLIENT_ID = "844275130627-7o60j8u9qoe3rj50m5sib5m7ik8rig9q.apps.googleusercontent.com";
 
-function testEmail(email) {
-	var url = window.href;
-	var domain = function domain(a) {
-		a = a.split('@');
-		a = a[1].split('.');
-		if (a[0] === 'archermalmo') {
-			return true;
-		} else {
-			return false;
-		}
-	};
-	if (domain(email)) {
-		body.style.display = 'initial';
-	} else {
-		window.open('unauthorized.html', '_self');
-	}
+        gsi.addEventListener('google-signin-aware-success', function (data) {
+            readData(data);
+        });
+    })();
 }
